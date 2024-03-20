e204611f440aadc4b491ad2b43d786a0655565d1
SnehaDahat <161016537+SnehaDahat@users.noreply.github.com>
2024-03-20 20:34:52 +0530

    Add files via upload

---
+++ app.py123.py
@@ -0,0 +1,31 @@
+import flask 
+from flask import Flask, render_template, request
+import joblib
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/prediction' , methods = ['GET' , 'POST'])
+def prediction():
+    if request.method == 'POST':
+        rev = request.form.get('reviews')
+
+        data_point = [rev]
+
+        model = joblib.load('naive_bayes.pkl')
+
+        prediction = model.predict(data_point)
+
+        if prediction[0] == 1:
+            result = "😄 Positive Review 😄"
+        else:
+            result = "☹️ Negative Review ☹️"
+
+        return render_template('output.html', prediction=result)
+
+    return render_template('output.html', prediction="")
+
+if __name__ == '__main__':
+    app.run(debug=True)
